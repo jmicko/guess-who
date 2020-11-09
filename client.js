d490a0b8c09d0410c2ba7addf30a7594f2b986d9
John Micko <jrmicko5012@gmail.com>
2020-11-09 16:23:49 -0600

    jQuery connected

--- client.js
+++ client.js
@@ -1,1 +1,13 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(onReady);
+
+function onReady() {
+    appendPictures()
+}
+
+function appendPictures() {
+    for (const person of people) {
+        console.log(person);
+    }
+}
