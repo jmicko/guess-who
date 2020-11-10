f86168a23a956fa7586c0993e916717555b1370d
John Micko <jrmicko5012@gmail.com>
2020-11-10 03:36:01 -0600

    username is saved as data

--- client.js
+++ client.js
@@ -3,12 +3,23 @@ console.log('Here are all the available people:', people);
 $(document).ready(onReady);
 
 function onReady() {
-    appendPictures()
+    // add pictures after page load
+    appendPictures();
+    // start function that will choose a random person from the list
+    startGame();
+    // click listener to check if player guessed correctly
+    $('main').on('click', 'picture', checkGuess)
 }
 
 function appendPictures() {
     for (const person of people) {
         console.log(person);
-        $('main').append(`<div class="picture"><img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of Chris"></div>`)
+        $('main').append(`
+            <div class="picture" data-username = ${person.githubUsername}>
+                <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+            </div>
+        `);
     }
 }
+
+start
